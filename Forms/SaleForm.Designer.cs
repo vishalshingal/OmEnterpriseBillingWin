aa0a3883f575cb19d2f54f1a3367d4c5714453a1
vishalshingal <shingalav62@gmail.com>
2025-10-04 13:13:13 +0530

    test

--- Forms/SaleForm.Designer.cs
+++ Forms/SaleForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace OmEnterpriseBillingWin.Forms
+﻿namespace OmEnterpriseBillingWin
 {
-    partial class SaleForm
+    public partial class SaleForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBuyers = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboItems = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgvSaleItems = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSaleItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.Text = "Buyer";
+            // 
+            // cboBuyers
+            // 
+            this.cboBuyers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBuyers.FormattingEnabled = true;
+            this.cboBuyers.Location = new System.Drawing.Point(89, 12);
+            this.cboBuyers.Name = "cboBuyers";
+            this.cboBuyers.Size = new System.Drawing.Size(283, 23);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.Text = "Item";
+            // 
+            // cboItems
+            // 
+            this.cboItems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboItems.FormattingEnabled = true;
+            this.cboItems.Location = new System.Drawing.Point(89, 41);
+            this.cboItems.Name = "cboItems";
+            this.cboItems.Size = new System.Drawing.Size(283, 23);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.Text = "Quantity";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(89, 70);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(283, 23);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 15);
+            this.label4.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(89, 99);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(283, 23);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(297, 128);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgvSaleItems
+            // 
+            this.dgvSaleItems.AllowUserToAddRows = false;
+            this.dgvSaleItems.AllowUserToDeleteRows = false;
+            this.dgvSaleItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSaleItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSaleItems.Location = new System.Drawing.Point(12, 157);
+            this.dgvSaleItems.Name = "dgvSaleItems";
+            this.dgvSaleItems.ReadOnly = true;
+            this.dgvSaleItems.RowTemplate.Height = 25;
+            this.dgvSaleItems.Size = new System.Drawing.Size(360, 200);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(216, 363);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(297, 363);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblSubtotalLabel
+            // 
+            this.lblSubtotalLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSubtotalLabel.AutoSize = true;
+            this.lblSubtotalLabel.Location = new System.Drawing.Point(230, 370);
+            this.lblSubtotalLabel.Name = "lblSubtotalLabel";
+            this.lblSubtotalLabel.Size = new System.Drawing.Size(54, 15);
+            this.lblSubtotalLabel.Text = "Subtotal:";
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(290, 370);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(34, 15);
+            this.lblSubtotal.Text = "$0.00";
+            // 
+            // lblDiscountLabel
+            // 
+            this.lblDiscountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDiscountLabel.AutoSize = true;
+            this.lblDiscountLabel.Location = new System.Drawing.Point(230, 390);
+            this.lblDiscountLabel.Name = "lblDiscountLabel";
+            this.lblDiscountLabel.Size = new System.Drawing.Size(57, 15);
+            this.lblDiscountLabel.Text = "Discount:";
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.Location = new System.Drawing.Point(290, 390);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(34, 15);
+            this.lblDiscount.Text = "$0.00";
+            // 
+            // lblTotalLabel
+            // 
+            this.lblTotalLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalLabel.AutoSize = true;
+            this.lblTotalLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotalLabel.Location = new System.Drawing.Point(230, 410);
+            this.lblTotalLabel.Name = "lblTotalLabel";
+            this.lblTotalLabel.Size = new System.Drawing.Size(40, 15);
+            this.lblTotalLabel.Text = "Total:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(290, 410);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(37, 15);
+            this.lblTotal.Text = "$0.00";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(89, 67);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(102, 15);
+            this.lblStock.Text = "Available Stock: 0";
+            // 
+            // SaleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SaleForm";
+            this.ClientSize = new System.Drawing.Size(384, 450);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTotalLabel);
+            this.Controls.Add(this.lblDiscount);
+            this.Controls.Add(this.lblDiscountLabel);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.lblSubtotalLabel);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvSaleItems);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboItems);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboBuyers);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SaleForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "New Sale";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSaleItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBuyers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboItems;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvSaleItems;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblSubtotalLabel;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblDiscountLabel;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.Label lblTotalLabel;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblStock;
     }
 }
